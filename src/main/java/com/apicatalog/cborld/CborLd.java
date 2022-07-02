e0e7ad0140daa73d5f229342200e768e42b4025f
Filip <filip26@gmail.com>
2022-07-02 21:37:16 +0200

    Add CborLd API skeleton

---
+++ src/main/java/com/apicatalog/cborld/CborLd.java
@@ -0,0 +1,16 @@
+package com.apicatalog.cborld;
+
+import jakarta.json.JsonObject;
+
+public final class CborLd {
+
+    
+    public static final byte[] encode(JsonObject compactedDocument) {
+	return null;
+    }
+    
+    public static final JsonObject decode(byte[] encodedDocument) {
+	return null;
+    }
+    
+}
