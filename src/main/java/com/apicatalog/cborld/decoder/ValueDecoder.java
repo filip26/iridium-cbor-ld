1c6e80c2cd8ec22c0a493f48874463e616d686cf
Filip <filip26@gmail.com>
2022-07-27 17:56:16 +0200

    Introduce ValueDecoder interface

---
+++ src/main/java/com/apicatalog/cborld/decoder/ValueDecoder.java
@@ -0,0 +1,12 @@
+package com.apicatalog.cborld.decoder;
+
+import com.apicatalog.cborld.dictionary.Dictionary;
+
+import co.nstant.in.cbor.model.DataItem;
+import jakarta.json.JsonValue;
+
+public interface ValueDecoder {
+
+    JsonValue decode(Dictionary dictionary, DataItem value );
+    
+}
