abd6f781832df8048e513d6e5a9e06c2edda5cd9
Filip <filip26@gmail.com>
2022-07-27 23:19:41 +0200

    Implement IdValueEncoder

--- src/main/java/com/apicatalog/cborld/encoder/value/IdValueEncoder.java
+++ src/main/java/com/apicatalog/cborld/encoder/value/IdValueEncoder.java
@@ -1,48 +1,28 @@
 package com.apicatalog.cborld.encoder.value;
 
-import com.apicatalog.cborld.dictionary.CodecTermMap;
+import java.math.BigInteger;
+
 import com.apicatalog.cborld.dictionary.Dictionary;
 import com.apicatalog.json.cursor.JsonValueCursor;
 import com.apicatalog.jsonld.context.TermDefinition;
+import com.apicatalog.jsonld.lang.Keywords;
 
 import co.nstant.in.cbor.model.DataItem;
+import co.nstant.in.cbor.model.UnsignedInteger;
 
 public class IdValueEncoder implements ValueEncoder {
 
     @Override
     public DataItem encode(Dictionary dictionary, JsonValueCursor value, String term, TermDefinition def) {
-	// TODO Auto-generated method stub
+	
+	if (def != null && Keywords.ID.equals(def.getUriMapping())) {
+	    
+	    final BigInteger code = dictionary.getCode(value.stringValue());
+			    
+	    if (code != null) {
+		return new UnsignedInteger(code);
+	    } 
+	}
 	return null;
-    }
-
-    /*
-
-	    System.out.println(">>> " + value);
-	    System.out.println("  > " + def);
-	    if (def != null) {	    
-        	    if (Keywords.CONTEXT.equals(def.getUriMapping())) {	    
-        		final byte[] code = contexts.getCode(value.stringValue());
-        		
-        		if (code != null) {
-        		    return new UnsignedInteger(new BigInteger(code));
-        		}
-        		
-        	    } else if (Keywords.TYPE.equals(def.getUriMapping())) {
-        		final Integer code = index.getCode(value.stringValue());
-        				    
-        		if (code != null) {
-        		    return new UnsignedInteger(code);
-        		}		    
-        		    
-        	    } else if (Keywords.ID.equals(def.getUriMapping())) {
-        		final Integer code = index.getCode(value.stringValue());
-        				    
-        		if (code != null) {
-        		    return new UnsignedInteger(code);
-        		} 
-        	    }
-
-
-     */
-    
+    }    
 }
