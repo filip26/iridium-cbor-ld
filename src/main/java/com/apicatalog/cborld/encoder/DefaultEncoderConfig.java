2d834586cd7b4678f669c0d33d905d6166ba9d20
Filip <filip26@gmail.com>
2022-07-28 18:37:45 +0200

    Hook-up StringValueEncoder

--- src/main/java/com/apicatalog/cborld/encoder/DefaultEncoderConfig.java
+++ src/main/java/com/apicatalog/cborld/encoder/DefaultEncoderConfig.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import com.apicatalog.cborld.dictionary.ContextDictionary;
 import com.apicatalog.cborld.encoder.value.ContextValueEncoder;
 import com.apicatalog.cborld.encoder.value.IdValueEncoder;
+import com.apicatalog.cborld.encoder.value.StringValueEncoder;
 import com.apicatalog.cborld.encoder.value.TypeValueEncoder;
 import com.apicatalog.cborld.encoder.value.UuidValueEncoder;
 import com.apicatalog.cborld.encoder.value.ValueEncoder;
@@ -23,6 +24,7 @@ public class DefaultEncoderConfig implements EncoderConfigration {
         VALUE_ENCODERS.add(new XsdDateTimeValueEncoder());
         VALUE_ENCODERS.add(new XsdDateValueEncoder());
         VALUE_ENCODERS.add(new UuidValueEncoder());
+        VALUE_ENCODERS.add(new StringValueEncoder());
     }
 
     public static final boolean COMPACT_ARRAYS = true;
