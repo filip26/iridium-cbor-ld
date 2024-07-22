5926862de68ee614ed67383a7cc103918b60966d
Filip <filip26@gmail.com>
2024-07-22 18:11:56 +0200

    Improve terms range check

--- src/main/java/com/apicatalog/cborld/dictionary/ContextDictionary.java
+++ src/main/java/com/apicatalog/cborld/dictionary/ContextDictionary.java
@@ -9,7 +9,9 @@ import com.apicatalog.cborld.hex.Hex;
 /**
  * A registry of well-know term values as defined by the specification.
  *
- * @see <a href="https://digitalbazaar.github.io/cbor-ld-spec/#term-codec-registry">Term Codeec Registry</a>
+ * @see <a href=
+ *      "https://digitalbazaar.github.io/cbor-ld-spec/#term-codec-registry">Term
+ *      Codeec Registry</a>
  *
  */
 public class ContextDictionary implements Dictionary {
@@ -17,23 +19,23 @@ public class ContextDictionary implements Dictionary {
     private static final byte OFFSET = 0x10;
 
     private static final String[] TERMS = new String[] {
-        "https://www.w3.org/ns/activitystreams",
-        "https://www.w3.org/2018/credentials/v1",
-        "https://www.w3.org/ns/did/v1",
-        "https://w3id.org/security/suites/ed25519-2018/v1",
-        "https://w3id.org/security/suites/ed25519-2020/v1",
-        "https://w3id.org/cit/v1",
-        "https://w3id.org/age/v1",
-        "https://w3id.org/security/suites/x25519-2020/v1",
-        "https://w3id.org/veres-one/v1",
-        "https://w3id.org/webkms/v1",
-        "https://w3id.org/zcap/v1",
-        "https://w3id.org/security/suites/hmac-2019/v1",
-        "https://w3id.org/security/suites/aes-2019/v1",
-        "https://w3id.org/vaccination/v1",
-        "https://w3id.org/vc-revocation-list-2020/v1",
-        "https://w3id.org/dcc/v1",
-        "https://w3id.org/vc/status-list/v1",
+            "https://www.w3.org/ns/activitystreams",
+            "https://www.w3.org/2018/credentials/v1",
+            "https://www.w3.org/ns/did/v1",
+            "https://w3id.org/security/suites/ed25519-2018/v1",
+            "https://w3id.org/security/suites/ed25519-2020/v1",
+            "https://w3id.org/cit/v1",
+            "https://w3id.org/age/v1",
+            "https://w3id.org/security/suites/x25519-2020/v1",
+            "https://w3id.org/veres-one/v1",
+            "https://w3id.org/webkms/v1",
+            "https://w3id.org/zcap/v1",
+            "https://w3id.org/security/suites/hmac-2019/v1",
+            "https://w3id.org/security/suites/aes-2019/v1",
+            "https://w3id.org/vaccination/v1",
+            "https://w3id.org/vc-revocation-list-2020/v1",
+            "https://w3id.org/dcc/v1",
+            "https://w3id.org/vc/status-list/v1",
     };
 
     private static final Map<Integer, Integer> CODEC_INDEX = new HashMap<>();
@@ -47,30 +49,30 @@ public class ContextDictionary implements Dictionary {
     @Override
     public BigInteger getCode(String term) {
         Integer code = CODEC_INDEX.get(term.hashCode());
-    
+
         if (code != null) {
             return BigInteger.valueOf(code);
         }
-    
+
         return null;
     }
-    
+
     @Override
     public String getValue(BigInteger code) {
-    
-        final int value = code.intValueExact();
-    
-        if (value < OFFSET) {
+
+        final int index = code.intValueExact() - OFFSET;
+
+        if (index < 0 || index >= TERMS.length) {
             throw new IllegalArgumentException(
-                        "Invalid code " + Hex.toString(code.toByteArray())  + "."
-                        + "Available range "
-                                + Hex.toString(OFFSET)
-                                + "-"
-                                + Hex.toString((byte)(OFFSET + TERMS.length))
-                                + "."
-                            );
+                    "Invalid code=" + Hex.toString(code.toByteArray()) + ", offset=" + Hex.toString(OFFSET) + "."
+                            + "Available range "
+                            + Hex.toString(OFFSET)
+                            + "-"
+                            + Hex.toString((byte) (OFFSET + TERMS.length))
+                            + ".");
         }
-        return TERMS[value - OFFSET];
+
+        return TERMS[index];
     }
 
     public String getTerm(Integer code) {
