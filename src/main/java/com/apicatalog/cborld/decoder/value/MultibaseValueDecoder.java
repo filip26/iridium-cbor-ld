d87c3b6fcc8b3242525291ac76dc10b5a085dfff
Filip <filip26@gmail.com>
2024-07-23 21:04:10 +0200

    Generalize MultibaseValueDecoder

--- src/main/java/com/apicatalog/cborld/decoder/value/MultibaseValueDecoder.java
+++ src/main/java/com/apicatalog/cborld/decoder/value/MultibaseValueDecoder.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import com.apicatalog.cborld.decoder.DecoderError;
 import com.apicatalog.cborld.dictionary.Dictionary;
 import com.apicatalog.cborld.encoder.value.MultibaseValueEncoder;
-import com.apicatalog.multibase.Multibase;
+import com.apicatalog.multibase.MultibaseDecoder;
 
 import co.nstant.in.cbor.model.ByteString;
 import co.nstant.in.cbor.model.DataItem;
@@ -14,30 +14,35 @@ import jakarta.json.Json;
 import jakarta.json.JsonValue;
 
 public class MultibaseValueDecoder implements ValueDecoder {
-    
-    
+
+    static final MultibaseDecoder MULTIBASE = MultibaseDecoder.getInstance();
+
     @Override
     public JsonValue decode(Dictionary dictionary, DataItem value, String term, Collection<String> types) throws DecoderError {
 
         if (types != null && types.contains(MultibaseValueEncoder.TYPE)
-                && MajorType.BYTE_STRING.equals(value.getMajorType())
-                ) {
+                && MajorType.BYTE_STRING.equals(value.getMajorType())) {
 
-            byte[] byteString = ((ByteString)value).getBytes();
-            
-            if (byteString.length <= 1 || byteString[0] != 'z') {
+            byte[] byteString = ((ByteString) value).getBytes();
+
+            if (byteString.length <= 1) {
                 return null;
             }
-            
-            
-            byte[] data = new byte[byteString.length - 1];
-            System.arraycopy(byteString, 1, data, 0, byteString.length - 1);
-            
-            String encoded = Multibase.BASE_58_BTC.encode(data);
 
-            if (encoded != null) {
-                return Json.createValue(encoded);
-            }
+            return MULTIBASE.getBase((char) byteString[0])
+                    .map(base -> {
+                        byte[] data = new byte[byteString.length - 1];
+                        System.arraycopy(byteString, 1, data, 0, byteString.length - 1);
+
+                        String encoded = base.encode(data);
+                        if (encoded != null) {
+                            return Json.createValue(encoded);
+                        }
+                        
+                        return null;
+
+                    })
+                    .orElse(null);
         }
         return null;
     }
