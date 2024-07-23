921fa6d0c41151964231fcca0a5c28b09a6bbd54
Filip <filip26@gmail.com>
2024-07-23 21:01:10 +0200

    Generalize MultibaseValueEncoder

--- src/main/java/com/apicatalog/cborld/encoder/value/MultibaseValueEncoder.java
+++ src/main/java/com/apicatalog/cborld/encoder/value/MultibaseValueEncoder.java
@@ -4,34 +4,39 @@ import java.util.Collection;
 
 import com.apicatalog.cborld.dictionary.Dictionary;
 import com.apicatalog.cursor.ValueCursor;
-import com.apicatalog.multibase.Multibase;
 import com.apicatalog.multibase.MultibaseDecoder;
 
 import co.nstant.in.cbor.model.ByteString;
 import co.nstant.in.cbor.model.DataItem;
 
 public class MultibaseValueEncoder implements ValueEncoder {
-    
+
     public static final String TYPE = "https://w3id.org/security#multibase";
-    
-    protected static final MultibaseDecoder MULTIBASE = MultibaseDecoder.getInstance(Multibase.BASE_58_BTC);
-    
+
+    static final MultibaseDecoder MULTIBASE = MultibaseDecoder.getInstance();
+
     @Override
     public DataItem encode(Dictionary dictionary, ValueCursor value, String term, Collection<String> types) {
-
         if (value.isString() && types != null && types.contains(TYPE)) {
 
-            byte[] bytes = MULTIBASE.decode(value.stringValue());
-            if (bytes != null) {
-                
-                byte[] compressed = new byte[bytes.length + 1];
-                
-                compressed[0] = 'z';
-                
-                System.arraycopy(bytes, 0, compressed, 1, bytes.length);
-                
-                return new ByteString(compressed);
-            }            
+            final String based = value.stringValue();
+
+            return MULTIBASE.getBase(based)
+                    .map(base -> {
+                        byte[] bytes = base.decode(value.stringValue());
+                        if (bytes != null) {
+
+                            byte[] compressed = new byte[bytes.length + 1];
+
+                            compressed[0] = (byte) base.prefix();
+
+                            System.arraycopy(bytes, 0, compressed, 1, bytes.length);
+
+                            return new ByteString(compressed);
+                        }
+                        return null;
+                    })
+                    .orElse(null);
         }
         return null;
     }
