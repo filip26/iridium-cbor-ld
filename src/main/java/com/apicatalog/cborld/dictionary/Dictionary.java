42e5b781daf6910f86d4b2aaf5c3dfa15bc1a717
Filip <filip26@gmail.com>
2024-07-26 22:26:36 +0200

    Minor improvements

--- src/main/java/com/apicatalog/cborld/dictionary/Dictionary.java
+++ src/main/java/com/apicatalog/cborld/dictionary/Dictionary.java
@@ -2,16 +2,10 @@ package com.apicatalog.cborld.dictionary;
 
 import java.util.Map;
 
-/**
- *
- *
- *
- */
 public interface Dictionary extends Iterable<Map.Entry<Integer, String>> {
 
     Integer getCode(String value);
 
     String getValue(Integer code);
 
-    
 }
