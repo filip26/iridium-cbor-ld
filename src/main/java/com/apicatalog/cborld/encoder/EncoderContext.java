d7240842b2be75c59aacffe799544433be1cce5a
Filip <filip26@gmail.com>
2025-07-30 19:16:36 +0200

    Adopt new maven central

--- src/main/java/com/apicatalog/cborld/encoder/EncoderContext.java
+++ src/main/java/com/apicatalog/cborld/encoder/EncoderContext.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 import com.apicatalog.jsonld.json.JsonUtils;
 import com.apicatalog.jsonld.lang.Keywords;
 import com.apicatalog.jsonld.uri.UriUtils;
+import com.apicatalog.jsonld.uri.UriValidationPolicy;
 
 import jakarta.json.JsonObject;
 import jakarta.json.JsonString;
@@ -21,47 +22,47 @@ public class EncoderContext {
     static final Collection<String> get(final JsonObject document, Collection<String> contexts) {
 
         for (final Entry<String, JsonValue> entry : document.entrySet()) {
-    
+
             if (Keywords.CONTEXT.equals(entry.getKey())) {
                 processContextValue(entry.getValue(), contexts);
 
             } else if (JsonUtils.isObject(entry.getValue())) {
                 get(entry.getValue().asJsonObject(), contexts);
             }
-        }    
+        }
         return contexts;
     }
 
     static final void processContextValue(final JsonValue jsonValue, final Collection<String> result) {
-    
+
         if (JsonUtils.isString(jsonValue)) {
-            final String uri = ((JsonString)jsonValue).getString();
-    
-            if (UriUtils.isAbsoluteUri(uri, true)) {
+            final String uri = ((JsonString) jsonValue).getString();
+
+            if (UriUtils.isAbsoluteUri(uri, UriValidationPolicy.Full)) {
                 result.add(uri);
                 return;
             }
-    
+
         } else if (JsonUtils.isNonEmptyArray(jsonValue)) {
-    
+
             for (final JsonValue item : jsonValue.asJsonArray()) {
-                processContextValue(item, result);                
-            }            
+                processContextValue(item, result);
+            }
             return;
-    
+
         } else if (JsonUtils.isObject(jsonValue)) {
-    
+
             if (jsonValue.asJsonObject().size() == 1 && JsonUtils.isString(jsonValue.asJsonObject().get(Keywords.ID))) {
-    
-                final String id = ((JsonString)jsonValue.asJsonObject().get(Keywords.ID)).getString();
-        
-                if (UriUtils.isAbsoluteUri(id, true)) {
+
+                final String id = ((JsonString) jsonValue.asJsonObject().get(Keywords.ID)).getString();
+
+                if (UriUtils.isAbsoluteUri(id, UriValidationPolicy.Full)) {
                     result.add(id);
                     return;
                 }
             }
         }
-    
+
         throw new IllegalArgumentException("Non compress-able context detected.");
     }
 }
