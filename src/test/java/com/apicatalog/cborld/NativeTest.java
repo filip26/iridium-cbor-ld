c00853cc74b39937945c860a19b6e658142f4711
Filip <filip26@gmail.com>
2025-11-25 19:21:16 +0100

    Improve example, add simple Map.of decode/encode test

--- src/test/java/com/apicatalog/cborld/NativeTest.java
+++ src/test/java/com/apicatalog/cborld/NativeTest.java
@@ -1,6 +1,7 @@
 package com.apicatalog.cborld;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -14,9 +15,11 @@ import com.apicatalog.cborld.decoder.Decoder;
 import com.apicatalog.cborld.decoder.DecoderException;
 import com.apicatalog.cborld.encoder.EncoderException;
 import com.apicatalog.cborld.registry.DocumentDictionary;
+import com.apicatalog.jsonld.Comparison;
 import com.apicatalog.jsonld.Document;
 import com.apicatalog.jsonld.loader.DocumentLoader;
 import com.apicatalog.jsonld.loader.StaticLoader;
+import com.apicatalog.multibase.Multibase;
 import com.apicatalog.tree.io.TreeIO;
 import com.apicatalog.tree.io.java.NativeAdapter;
 
@@ -43,44 +46,58 @@ public class NativeTest {
             .uri("did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK", 1)
             .build();
 
+    static Map<String, Object> jsonld = Map.of(
+            "@context", "https://apicatalog/example-context.jsonld",
+            "@type", "Person",
+            "name", "Filip Kolařík",
+            "project", "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK");
+
+    static String cborld = "FD9CB1D82187BA4000102186418666F46696C6970204B6F6C61C599C3AD6B186801";
+
     @Test
     void testMapEncode() throws EncoderException, DecoderException, CborException, IOException {
 
         var encoder = CborLd.newEncoder()
                 .loader(loader)
-//                .dictionary(dictionary)
+                .dictionary(dictionary)
                 .build();
 
-        Map<String, Object> jsonld = Map.of(
-                "@context", "https://apicatalog/example-context.jsonld",
-                "@type", "Person",
-                "name", "Filip Kolařík",
-                "project", "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK");
+        var result = encoder.encode(jsonld);
 
-        var cborld = encoder.encode(jsonld);
+        assertNotNull(result);
 
-        assertNotNull(cborld);
+        var match = cborld.equals(Multibase.BASE_16_UPPER.encode(result));
 
-        var writer = new StringWriter();
+        if (!match) {
+            var writer = new StringWriter();
+            writer.write("Expected:\n");
+            
+            CborWriter cborWriter = new CborWriter(writer);
+            cborWriter.write(CborDecoder.decode(Multibase.BASE_16_UPPER.decode(cborld)));
+            writer.flush();
+            writer.write("\n\nActual:\n");
+            cborWriter.write(CborDecoder.decode(result));
 
-        CborWriter cborWriter = new CborWriter(writer);
+            System.out.println(writer.toString());
+        }
 
-        final var decodedResult = CborDecoder.decode(cborld);
-
-        cborWriter.write(decodedResult);
-
-        System.out.println(writer.toString());
-
-        var x = Decoder.newBuilder(Version.V1)
-                .loader(loader)
-                .dictionary(dictionary)
-                .build()
-                .decode(cborld);
-
-        System.out.println(jsonld.toString().length());
-        System.out.println(cborld.length);
-        System.out.println(jsonld.toString());
-        System.out.println(x);
+        assertTrue(match);
     }
 
+    @Test
+    void testMapDecode() throws EncoderException, DecoderException, CborException, IOException {
+
+        var decoder = Decoder.newBuilder(Version.V1)
+                .loader(loader)
+                .dictionary(dictionary)
+                .build();
+
+        var result = decoder.decode(Multibase.BASE_16_UPPER.decode(cborld));
+
+        assertNotNull(result);
+
+        var match = Comparison.equals(jsonld, result, NativeAdapter.instance());
+
+        assertTrue(match);
+    }
 }
